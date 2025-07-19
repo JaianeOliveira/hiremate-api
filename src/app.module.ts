aa7eb4c99bf5d2d09ab4abd3679b4db2eca61f45
Jaiane Oliveira <jaianeoliveira.dev@gmail.com>
2025-07-19 01:08:19 -0300

    trying to fix error on deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ThrottlerModule } from '@nestjs/throttler';
 
 import { ScheduleModule } from '@nestjs/schedule';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
 import { ApplicationsModule } from './applications/applications.module';
 import { AuthModule } from './auth/auth.module';
 import { PrismaModule } from './prisma/prisma.module';
@@ -19,7 +21,7 @@ import { UsersModule } from './users/users.module';
     ThrottlerModule.forRoot([{ ttl: 3600, limit: 1000 }]),
     ScheduleModule.forRoot(),
   ],
-  controllers: [],
-  providers: [SchedulerService],
+  controllers: [AppController],
+  providers: [SchedulerService, AppService],
 })
 export class AppModule {}
